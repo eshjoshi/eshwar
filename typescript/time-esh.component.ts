b602426cba9b9105dfbd4fab00d1e388021b01c2
eshjoshi <eshwarjoshi3@gmail.com>
2018-07-03 20:23:31 +0530

    Add files via upload
    3/7

--- typescript/time-esh.component.ts
+++ typescript/time-esh.component.ts
@@ -15,6 +15,12 @@ export class TimeEshComponent implements OnInit {
    diffHours;                   // Differnce hour
    diffMinite;                  // Differnce minite
    diffSecond;                  // Differnce second
+   videoStart = false;
+   superVideos;
+   videoUrl;
+   date3;
+    date4;
+    one = 3;
 
 
    form = new FormGroup({
@@ -50,21 +56,34 @@ export class TimeEshComponent implements OnInit {
           let setDate = this.form.value.date;            // get date 
           let setTime = this.form.value.time;            // get time
           console.log( setDate +  ","  +setTime);
-          let val =  setDate +  ","  +setTime;           // arrange in one variable
+          let val =  setDate +  ","  +setTime;    
+          console.log(val);       // arrange in one variable
           if(setDate && setTime != "Enter Time"){
             var date2 = new Date(val); 
+
+             
           }
          // value convert to the date
           console.log("date1:-" + date2);
           this.date2 = date2;
+
+       
     }
+ 
+      
+
+
+
   
   clock(){     
       let currentTime = new Date();
-      this.currentTime =  currentTime;   
+      this.currentTime =  currentTime; 
+     
        if(this.date2){
+          
          this.logic();
        }
+ 
    }
 
 
@@ -88,17 +107,63 @@ export class TimeEshComponent implements OnInit {
       this.diffMinite = diffMinite;
       this.diffSecond = diffSecond;  
 
-      if( this.diffSecond == 0){
-        alert("Time UP");
+
+       //  this.everyday(this.diffSecond);
+         
+      if( this.diffSecond == 0  ){
+        this.videoStart = true;
+          var one = this.one;
+          var two = this.currentTime.getDate();
+         
+        if(one == two ){
+          this.videoUrl = "assets/video/mov_bbb_" + one + ".mp4";
+        }
+           
+        // this.superVideos = [];
+        // for(let i = 1; i<=this.diffMinite; i++){
+        //   //  this.videoUrl = "./../assets/video/mov_bbb_" + i + ".mp4";
+        //   let videoUrl = "assets/video/mov_bbb_" + i + ".mp4";
+        //   this.superVideos.push(videoUrl);
+
+        //   //console.log(i);
+        // }
+        // console.log(this.superVideos);
+      //  alert("Time UP");
       }
+      else if(this.diffSecond == 3600){
+          this.videoStart = false;
+           
+           this.one = one++;
+
+         }
+  
    }
+
+    //  everyday(diff){
+
+    //     // this.date3 = new Date("2018-07-03,07:55");
+    //     // this.date4 = new Date();
+       
+    //     // var one = this.date3.getDate();
+    //     // var two = this.date4.getDate();
+       
+    //       this.videoStart = true;
+    //       this.videoUrl = "assets/video/mov_bbb_" + diff + ".mp4";
+        
+    // }
+
+    
 
 
 
   ngOnInit() {
+
+   
       setInterval(()=> {
         this.clock();
       }, 1000);  
+
+   
       }
 
 }
